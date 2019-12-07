c3edf888a5db58fa4c2aad869e7aacd8037b1d3b
dudl2547 <tonydudley23@gmail.com>
2019-12-06 21:06:51 -0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+# Download the latest Time Management Image
+module "image" {
+  source = "./image"
+  image  = "${var.image}"
+}
+
+# Start the Container
+module "container" {
+  source   = "./container"
+  image    = "${module.image.image_out}"
+  name     = "${var.container_name}"
+  int_port = "${var.int_port}"
+  ext_port = "${var.ext_port}"
+}
